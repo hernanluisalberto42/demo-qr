d07f7f06416e9a3a13e8eb0fac16b95b97f629d6
hernanluisalberto42 <hernanluisalberto42@gmail.com>
2023-02-08 23:27:45 -0500

    add export qr con logo

--- src/main/java/com/example/qr/demoqr/controller/CodeController.java
+++ src/main/java/com/example/qr/demoqr/controller/CodeController.java
@@ -1,11 +1,13 @@
 package com.example.qr.demoqr.controller;
 
 import com.example.qr.demoqr.utils.GenerateCode;
+import com.google.zxing.WriterException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/generate")
@@ -15,5 +17,17 @@ public class CodeController {
     public ResponseEntity<BufferedImage> codeQr(@RequestBody String barcode)
             throws Exception {
         return ResponseEntity.ok(GenerateCode.generateQRCodeImage(barcode));
+    }
+
+    @GetMapping("/qr/export/{message}")
+    public ResponseEntity<String> exportQr(@PathVariable String message) throws IOException, WriterException {
+        GenerateCode.exportQRCode(message);
+        return ResponseEntity.ok("Qr created with successfully");
+    }
+
+    @GetMapping("/qr/export/logo/{message}")
+    public ResponseEntity<String> exportQrLogo(@PathVariable String message) throws IOException, WriterException {
+        GenerateCode.exportQrCodeLogo(message);
+        return ResponseEntity.ok("Qr created with successfully");
     }
 }
--- src/main/java/com/example/qr/demoqr/utils/GenerateCode.java
+++ src/main/java/com/example/qr/demoqr/utils/GenerateCode.java
@@ -1,17 +1,27 @@
 package com.example.qr.demoqr.utils;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.oned.EAN13Writer;
 import net.glxn.qrgen.javase.QRCode;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class GenerateCode {
+
+    private static final String PATH="C:\\QR\\qrCreated.jpg";
 
     public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
         ByteArrayOutputStream stream = QRCode
@@ -28,6 +38,28 @@ public class GenerateCode {
         BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, 300, 150);
 
         return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+    public static void exportQRCode(String message) throws WriterException, IOException {
+        BitMatrix bitMatrix= new MultiFormatWriter().encode(message,BarcodeFormat.QR_CODE,500,500);
+        MatrixToImageWriter.writeToPath(bitMatrix,"jpg", Path.of(PATH));
+    }
+
+    public static void exportQrCodeLogo(String message) throws IOException, WriterException {
+        BitMatrix bitMatrix= new MultiFormatWriter().encode(message,BarcodeFormat.QR_CODE,400,400);
+        MatrixToImageConfig imageConfig=new MatrixToImageConfig(MatrixToImageConfig.BLACK,MatrixToImageConfig.WHITE);
+
+        BufferedImage qrImage=MatrixToImageWriter.toBufferedImage(bitMatrix,imageConfig);
+        BufferedImage logoImage=ImageIO.read(new File("C:\\QR\\pencil.png"));
+        int finalImageHeight=qrImage.getHeight() - logoImage.getHeight();
+        int finalImageWidth=qrImage.getWidth() - logoImage.getWidth();
+
+        BufferedImage finalImage= new BufferedImage(qrImage.getHeight(),qrImage.getWidth(),BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics= (Graphics2D) finalImage.getGraphics();
+        graphics.drawImage(qrImage,0,0,null);
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,1f));
+        graphics.drawImage(logoImage,(int) Math.round(finalImageWidth / 2),(int) Math.round(finalImageHeight / 2), null);
+        ImageIO.write(finalImage,"png",new File("C:\\QR\\qrWithLogo.png"));
     }
 
 }
